ce4ef9a11f978648bf541c97b3205d7c889ebe1e
Prannaya <prannayagupta@gmail.com>
2023-10-21 15:05:36 +0800

    v1.5.3: stylise ctf page :slightly_smiling_face:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'nuxt-swiper', '@nuxt/image', '@nuxtjs/robots', 'nuxt-simple-sitemap', 'nuxt-gtag', 'nuxt-clarity-analytics'],
   app: {
     head: {
-      link: [{ rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' }],
+      link: [
+        { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=League Spartan' }
+      ],
       meta: [
         { name: 'theme-color', content: '#ffffff', media: '(prefers-color-scheme: light)' },
         { name: 'theme-color', content: '#1e293b', media: '(prefers-color-scheme: dark)' },
--- pages/events/ctf.vue
+++ pages/events/ctf.vue
@@ -7,20 +7,47 @@
                     <PatchesLarger />
                     <div class="mx-auto lg:max-w-7xl lg:flex justify-center max-w-lg px-8 sm:px-0">
                         <div class="text-left sm:pb-8 max-w-xl pb-10">
-                            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-white">
-                                BBCS × BlahajCTF</h1>
-                            <h2 class="text-3xl mt-6 font-semibold tracking-tight text-gray-900 dark:text-white">
-                                Key Details</h2>
+                            <div class="mb-8 md:mb-0">
+                              <div class="flex items-center justify-center md:pb-2">
+                                <nuxt-img format="webp" src="/logo.png" class="mr-1 h-8" alt="Our Logo" />
+                                <span
+                                    class="self-center text-2xl mr-3 font-semibold whitespace-nowrap dark:text-white">
+                                    BuildingBloCS
+                                </span>
+                                <span class="ctf-subtitle text-medium font-medium text-gray-900 dark:text-white">
+                                    PRESENTS
+                                </span>
+                              </div>
+                            </div>
+                            <h1 class="text-center ctf-title text-7xl font-bold text-gray-900 sm:text-9xl dark:text-white">
+                                blahaj ctf
+                            </h1>
+                            <h2 class="text-center ctf-subtitle text-md sm:text-lg font-medium text-gray-900 dark:text-white">
+                                2ND DECEMBER • 9AM - 9PM • ONLINE
+                            </h2>
+
+                            <p class="text-center mt-3 text-lg leading-8 text-gray-600 dark:text-slate-300">
+                                BuildingBloCS's collaboration with Team Blahaj brings you... 
+                                <b>BlahajCTF</b>! This beginner-friendly capture-the-flag 
+                                tournament is for all those interested in cybersecurity, fun 
+                                and BLÅHAJ!
+                            </p>
+
+                            <p class="text-center text-sm sm:text-md leading-8 ctf-code font-medium">
+                                <span class="font-extrabold">$</span> PING <a href="https://go.buildingblocs.sg/ctf/discord" class="hover:underline hover:font-extrabold">GO.BUILDINGBLOCS.SG/CTF/DISCORD</a>
+                            </p>
+
+                            <!-- <h2 class="text-3xl mt-6 font-semibold tracking-tight text-gray-900 dark:text-white">
+                                Key Details
+                            </h2>
                             <p class="mt-3 text-lg leading-8 text-gray-600 dark:text-slate-300">
                                 Date: 2nd December <br>
                                 Venue: Online <br>
                                 Email contact for queries:
                                 <NuxtLink to="mailto:hello@buildingblocs.sg" class="text-orange-500 dark:text-orange-400 hover:underline">hello@buildingblocs.sg</NuxtLink>
-                            </p>
-                          <!-- <p class="mt-3 text-lg leading-8 text-gray-600 dark:text-slate-300 font-bold">Please note
-                            that the workshop "Intro to Vue" has been cancelled. We apologise for any inconvenience caused.</p> -->
-                            <p class="mt-3 text-lg leading-8 text-gray-600 dark:text-slate-300">BuildingBloCS's collaboration with Team Blahaj brings you... <b>BlahajCTF</b>! This for-beginners CTF will be happening on <b>2nd Dec</b> virtually.</p>
-                            <!-- <p class="mt-3 text-lg leading-8 text-gray-600 dark:text-slate-300">We have closed signups due to overwhelming demand. Please email us for any queries and make sure you're in the Discord channel to receive updates on the event. We hope to see you at our future events!</p> -->
+                            </p> -->
+                            
+                            
                             <div>
                                 <div class="mt-6">
                                     <Disclosure v-for="(faq, index) in faqs" :key="index" v-slot="{ open }">
@@ -42,45 +69,34 @@
                         <div class="mx-1 md:mx-7">
                             <div class="mx-1 md:mx-7 max-w-lg">
                                 <div class="mx-1 md:mx-7 max-w-lg">
-                                    <nuxt-img format="webp" src="/ctf/chair.png" alt=""
+                                    <nuxt-img format="webp" src="/ctf/poster.png" alt=""
                                         class="object-cover object-center w-full max-h-[25rem] lg:max-h-full rounded-xl shadow-xl ring-1 ring-gray-400/10"
                                     />
-                                    <!-- <div v-for="timeline in timelines" :key="timeline.date">
-                                        <h1
-                                            class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl dark:text-white">
-                                            {{ timeline.title }}
-                                        </h1>
-                                        <h2
-                                            class="text-2xl font-semibold tracking-tight text-gray-900 sm:text-3xl dark:text-white mb-4">
-                                            {{ timeline.date }}
-                                        </h2>
-                                        <ol class="relative border-l border-gray-200 dark:border-gray-700 mb-10">
-                                            <li v-for="event in timeline.events" :key="event.title" class="mb-10 ml-4">
-                                                <div
-                                                    class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-800 dark:bg-gray-600">
-                                                </div>
-                                                <time
-                                                    class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-400">{{
-                                                        event.date }}</time>
-                                                <h3
-                                                    class="text-lg font-semibold tracking-tight text-gray-900 sm:text-xl dark:text-white">
-                                                    {{ event.title }}
-                                                </h3>
-                                                <p class="sm:text-lg text-md text-gray-600 dark:text-slate-300">
-                                                    {{ event.desc }}
-                                                </p>
-                                                <p class="text-md text-gray-500 dark:text-slate-400">
-                                                    {{ event.author }}
-                                                </p>
-                                            </li>
-                                        </ol>
-                                    </div> -->
                                 </div>
                             </div>
                         </div>
 
                     </div>
-                    <EventsCta />
+                    
+                    <div class="surface-section px-4 py-8 md:px-6 lg:px-8 mt-12">
+                        <div class="text-700 text-center">
+                            <div class="text-blue-600 font-bold mb-3 flex justify-center items-center"><IconsDiscord /><span class="ml-2">POWERED BY DISCORD</span></div>
+                            <div class="text-900 font-bold text-5xl mb-3 text-gray-900 dark:text-white">
+                                Join our Beginner-Friendly CTF!
+                            </div>
+                            <div class="text-700 text-2xl mb-5 text-gray-600 dark:text-gray-400">
+                                For all those interested in Cybersecurity, Fun and Blahaj!
+                            </div>
+                            <div class="align-center text-center justify-center flex">
+                            <NuxtLink href="https://go.buildingblocs.sg/ctf/discord" 
+                                class="font-bold px-5 py-3 rounded-3xl w-36 bg-indigo-500 hover:bg-indigo-600 text-white flex justify-center items-center">
+                                <IconsDiscord /><span class="ml-2">Join Now</span>
+                                <!-- Sign Up! -->
+                            </NuxtLink>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <EventsCta /> -->
                     <Footer />
                 </div>
             </div>
@@ -157,3 +173,20 @@ useSeoMeta({
     twitterCard: 'summary_large_image'
 })
 </script>
+<style>
+.ctf-title {
+    font-family: 'League Spartan', 'Inter var', sans-serif;
+    letter-spacing: -3px;
+}
+
+.ctf-subtitle {
+    font-family: 'Inter var', sans-serif;
+    letter-spacing: 4px;
+}
+
+.ctf-code {
+    font-family: 'Consolas', monospace;
+    color: #FFAB00;
+    letter-spacing: 4px;
+}
+</style>
