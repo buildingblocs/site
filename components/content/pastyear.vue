2702bafe5da809f83f729bf13df5e5e8ae4c3701
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2024-02-27 21:09:49 +0800

    make component along with example page

---
+++ components/content/pastyear.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink :to=link class="not-prose relative">
+    <img :src=image class="h-[30rem] w-full object-cover rounded-xl shadow-sm hover:shadow-xl transition opacity-90 hover:opacity-100">
+    <div class="absolute bottom-5 left-5">
+      <h2>{{ year }}</h2>
+      <p>{{ desc }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup>
+const { props } = defineProps(["year", "desc", "link", "image"]);
+</script>
---
+++ content/past-years.md
@@ -0,0 +1,12 @@
+---
+title: 'Past Years'
+---
+
+::pastyear
+---
+year: 2020
+desc: hi
+link: https://google.com
+image: /june/img1.webp
+---
+::
