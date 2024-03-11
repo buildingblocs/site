69849113ed101cce2c35ecfcd94b2b001f8e01a9
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2024-03-11 10:32:56 +0000

    add padding and zoom for component

--- components/content/pastyear.vue
+++ components/content/pastyear.vue
@@ -1,10 +1,12 @@
 <template>
   <NuxtLink :to=link class="not-prose relative">
-    <img :src=image class="h-[30rem] w-full object-cover rounded-xl shadow-sm hover:shadow-xl transition opacity-90 hover:opacity-100">
+  <div class="h-[30rem] w-full  mb-10 overflow-hidden rounded-xl shadow-sm hover:shadow-xl transition">
+      <img :src=image class="h-full w-full object-cover transition opacity-90 hover:opacity-100 hover:scale-105">
     <div class="absolute bottom-5 left-5">
       <h2>{{ year }}</h2>
       <p>{{ desc }}</p>
     </div>
+  </div>
   </NuxtLink>
 </template>
 
