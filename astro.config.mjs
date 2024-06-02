15c0a9837beb4f2b9064e1bc46ce6249c696c46a
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2024-06-02 12:26:34 +0000

    remove redirect to old years

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: 'https://buildingblocs.sg',
   redirects: {
-    '/past-years': '/archive',
-    '/2017/[...slug]': 'https://old.buildingblocs.sg/2017/[...slug]',
-    '/2018/[...slug]': 'https://old.buildingblocs.sg/2018/[...slug]',
-    '/2019/[...slug]': 'https://old.buildingblocs.sg/2019/[...slug]',
-    '/2020/[...slug]': 'https://old.buildingblocs.sg/2020/[...slug]',
-    '/2021/[...slug]': 'https://old.buildingblocs.sg/2021/[...slug]',
-    '/2022/[...slug]': 'https://old.buildingblocs.sg/2022/[...slug]',
-    '/2023/[...slug]': 'https://old.buildingblocs.sg/2023-newnew/[...slug]',
+    '/past-years': '/archive'
   },
   integrations: [tailwind({
     applyBaseStyles: false
