bfb31252582744e2780bae7d1fb80c3c110fcffc
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2024-09-04 13:44:46 +0000

    add our teams page and general updates

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,9 +1,8 @@
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
 import {
     Sheet,
     SheetContent,
@@ -21,6 +20,10 @@ export default function Nav() {
         {
             title: "Our Partners",
             href: "/partners"
+        },
+        {
+            title: "Our Team",
+            href: "/team"
         }
     ]
 
@@ -47,33 +50,34 @@ export default function Nav() {
                         <a href='/partners' className="text-sm font-medium rounded-md px-2 transition hover:bg-white hover:text-black">
                             Our Partners
                         </a>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger className="text-sm font-medium rounded-md px-2 flex items-center gap-x-1 outline-0 group transition hover:bg-white hover:text-black data-[state=open]:bg-slate-700">Events<ChevronDownIcon className="transition group-data-[state=open]:rotate-180 group-data-[state=open]:mt-0.5" /></DropdownMenuTrigger>
-                            <DropdownMenuContent className="max-w-xs p-2 gap-y-2 flex flex-col mt-5">
+                        <a href='/team' className="text-sm font-medium rounded-md px-2 transition hover:bg-white hover:text-black">
+                            Our Team
+                        </a>
+                        <Popover>
+                            <PopoverTrigger className="translate-y-0 translate-x-0 text-sm font-medium rounded-md px-2 flex items-center gap-x-1 outline-0 group transition hover:bg-white hover:text-black data-[state=open]:bg-slate-700">Events<ChevronDownIcon className="transition group-data-[state=open]:rotate-180 group-data-[state=open]:mt-0.5" /></PopoverTrigger>
+                            <PopoverContent className="max-w-xs p-2 flex flex-col mt-5 bg-slate-900 border-slate-700 text-white">
                                 {events.map((event) => (
-                                    <DropdownMenuItem key={event.title}>
-                                        <a href={event.href} className="font-semibold">
-                                            {event.title}
-                                            <p className="font-normal">{event.desc}</p>
-                                        </a>
-                                    </DropdownMenuItem>
+                                    <a href={event.href} className="font-semibold hover:bg-slate-800 p-2 rounded-md text-sm">
+                                        {event.title}
+                                        <p className="font-normal">{event.desc}</p>
+                                    </a>
                                 ))}
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+                            </PopoverContent>
+                        </Popover>
                     </div>
                 </div>
-                <a className="h-16 justify-end flex sticky top-0 z-20" href="/events/june">
+                {/* <a className="h-16 justify-end flex sticky top-0 z-20" href="/events/june">
                     <div className="pl-5 pr-2.5 rounded-full mt-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring ring-1 ring-slate-800 flex items-center gap-x-4">
                         <p>June Conference</p>
                         <div className="bg-orange-500 hover:bg-orange-600 rounded-full p-1 px-1.5 transition flex gap-x-2 items-center font-semibold"><ArrowRightIcon className="w-5 h-5" /></div>
                     </div>
-                </a>
+                </a> */}
             </div>
             <div className="sm:hidden h-16 justify-end flex sticky top-0 z-20">
-                <a href="/events/june" className="px-6 py-2 rounded-full mt-4 mr-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring ring-1 ring-slate-800 flex items-center gap-x-4 text-sm">
+                {/* <a href="/events/june" className="px-6 py-2 rounded-full mt-4 mr-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring ring-1 ring-slate-800 flex items-center gap-x-4 text-sm">
                     <p>June Conference</p>
                     <div className="bg-orange-500 hover:bg-orange-600 rounded-full p-1 px-1.5 transition flex gap-x-2 items-center font-semibold"><ArrowRightIcon className="w-5 h-5" /></div>
-                </a>
+                </a> */}
                 <div className="px-6 py-2 rounded-full mt-4 mr-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring ring-1 ring-slate-800 flex items-center gap-x-4">
                     <a href="/">
                         <img src='/favicon.svg' alt="BuildingBloCS logo" className="w-5 h-5" />
