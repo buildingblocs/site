825da1469dc7734b4235b292b1405a36afa343a3
myat <69038584+icepcp@users.noreply.github.com>
2025-05-06 12:18:46 +0000

    add new sponsors

--- src/partners.ts
+++ src/partners.ts
@@ -2,8 +2,29 @@ import cys from "./img/partners/cys.png";
 import github from "./img/partners/github.svg";
 import moetext from "./img/partners/moetext.svg";
 import sutd from "./img/partners/sutd.webp";
+import okx from "./img/partners/okx.svg";
+import revenuecat from "./img/partners/revenuecat.svg";
+import yubico from "./img/partners/yubico.svg"
 
 export const curr_partners = [
+    {
+        src: okx,
+        title: "OKX",
+        desc: "OKX is a Seychelles-based cryptocurrency exchange. As of Jan 2025, OKX is the 4th largest cryptocurrency exchange in the world according to CoinMarketCap.",
+        website: "https://www.okx.com/"
+    },
+    {
+        src: yubico,
+        title: "Yubico",
+        desc: "Yubico, a global cybersecurity company and inventor of the YubiKey, offers the gold standard for phishing-resistant multi-factor authentication (MFA).",
+        website: "https://www.yubico.com/"
+    },
+    {
+        src: revenuecat,
+        title: "RevenueCat",
+        desc: "RevenueCat is a mobile subscription service. They provide a comprehensive toolset for developers to implement and manage in-app purchases and subscriptions.",
+        website: "https://www.revenuecat.com/"
+    },
     {
         src: cys,
         title: "Cyber Youth Singapore",
