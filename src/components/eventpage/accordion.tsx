689c966d453818e7034def9bf45e37a2610bc2a3
myat <69038584+icepcp@users.noreply.github.com>
2025-05-06 11:10:32 +0000

    update wording

--- src/components/eventpage/accordion.tsx
+++ src/components/eventpage/accordion.tsx
@@ -9,7 +9,9 @@ export default function Faq({ faq }: { faq: Array<{ q: string; a: string; }> }) 
     const faqs = faq.map((faq) =>
         <AccordionItem value={faq.q} key={faq.q}>
             <AccordionTrigger>{faq.q}</AccordionTrigger>
-            <AccordionContent>{faq.a}</AccordionContent>
+            <AccordionContent>
+                <div className="prose prose-invert" dangerouslySetInnerHTML={{ __html: faq.a }} />
+            </AccordionContent>
         </AccordionItem>);
     return (
         <Accordion type="multiple">{faqs}</Accordion>
