5bc06a9f92ff9cbbf97a7bd664fc7b3e82b5fa19
myat <69038584+icepcp@users.noreply.github.com>
2025-05-06 14:29:20 +0000

    update positioning of partners

--- src/partners.ts
+++ src/partners.ts
@@ -8,40 +8,46 @@ import yubico from "./img/partners/yubico.svg"
 
 export const curr_partners = [
     {
-        src: okx,
-        title: "OKX",
-        desc: "OKX is a Seychelles-based cryptocurrency exchange. As of Jan 2025, OKX is the 4th largest cryptocurrency exchange in the world according to CoinMarketCap.",
-        website: "https://www.okx.com/"
-    },
-    {
-        src: yubico,
-        title: "Yubico",
-        desc: "Yubico, a global cybersecurity company and inventor of the YubiKey, offers the gold standard for phishing-resistant multi-factor authentication (MFA).",
-        website: "https://www.yubico.com/"
-    },
-    {
-        src: revenuecat,
-        title: "RevenueCat",
-        desc: "RevenueCat is a mobile subscription service. They provide a comprehensive toolset for developers to implement and manage in-app purchases and subscriptions.",
-        website: "https://www.revenuecat.com/"
+        src: sutd,
+        title: "Singapore University of Technology and Design",
+        desc: "SUTD integrates design, AI and technology into a holistic, interdisciplinary education and research experience. This unique approach encourages our students to push the boundaries of innovating solutions to real-world problems.",
+        website: "https://github.com/",
+        position: 1
     },
     {
         src: cys,
         title: "Cyber Youth Singapore",
         desc: "Cyber Youth Singapore is a registered charity under the Charities Act. They empower youths with skills and opportunities to become trailblazers of our digital future.",
         website: "https://www.cyberyouth.sg/",
-    },
-    {
-        src: sutd,
-        title: "Singapore University of Technology and Design",
-        desc: "SUTD integrates design, AI and technology into a holistic, interdisciplinary education and research experience. This unique approach encourages our students to push the boundaries of innovating solutions to real-world problems.",
-        website: "https://github.com/",
+        position: 1
     },
     {
         src: moetext,
         title: "Ministry of Education",
         desc: "The Ministry of Education (MOE) of Singapore formulates and implements education policies on education structure, curriculum, pedagogy, and assessment.",
         website: "https://www.moe.gov.sg/",
+        position: 1
+    },
+    {
+        src: yubico,
+        title: "Yubico",
+        desc: "Yubico, a global cybersecurity company and inventor of the YubiKey, offers the gold standard for phishing-resistant multi-factor authentication (MFA).",
+        website: "https://www.yubico.com/",
+        position: 2
+    },
+    {
+        src: revenuecat,
+        title: "RevenueCat",
+        desc: "RevenueCat is a mobile subscription service. They provide a comprehensive toolset for developers to implement and manage in-app purchases and subscriptions.",
+        website: "https://www.revenuecat.com/",
+        position: 2
+    },
+    {
+        src: okx,
+        title: "OKX",
+        desc: "OKX is a Seychelles-based cryptocurrency exchange. As of Jan 2025, OKX is the 4th largest cryptocurrency exchange in the world according to CoinMarketCap.",
+        website: "https://www.okx.com/",
+        position: 2
     },
 ];
 
