9c4998476187a44789d5a17eb249a5fd2d17ea6b
LinelyQwert <raynerng12@gmail.com>
2025-10-02 01:10:43 +0800

    Removed the june conf thing in navbar for both mobile and desktop views. Restored march and june conf to events dropdown oops

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -34,19 +34,19 @@ export default function Nav() {
     const events = [
         {
             title: "December Conference",
-            href: "", // "/events/december",
+            href: "/", // "/events/december",
             desc: "Under Construction! Check back later."
         }, 
-        //{
-        //     title: "June Conference",
-        //     href: "/events/june",
-        //     desc: "The June Conference is an AI-centered conference designed for everyone",
-        // },
-        // {
-        //     title: "March Conference",
-        //     href: "/events/march",
-        //     desc: "The March Conference aims to introduce beginners to Python and Data Science"
-        // },
+        {
+            title: "June Conference",
+            href: "/events/june",
+            desc: "The June Conference is an AI-centered conference designed for everyone",
+        },
+        {
+            title: "March Conference",
+            href: "/events/march",
+            desc: "The March Conference aims to introduce beginners to Python and Data Science"
+        },
         {
             title: "Looking for older events?",
             href: "/archive",
@@ -110,23 +110,23 @@ export default function Nav() {
                         </Popover>
                     </div>
                 </div>
-                {
+                {/* {
                     <a
                         className="h-16 justify-end flex sticky top-0 z-20"
-                        href="/events/june"
+                        href="/events/december"
                     >
                         <div className="pl-5 pr-2.5 rounded-full mt-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring-1 ring-slate-800 flex items-center gap-x-4">
-                            <p>June Conference</p>
+                            <p>December Conference</p>
                             <div className="bg-orange-500 hover:bg-orange-600 rounded-full p-1 px-1.5 transition flex gap-x-2 items-center font-semibold">
                                 <ArrowRightIcon className="w-5 h-5" />
                             </div>
                         </div>
                     </a>
-                }
+                } */}
             </div>
             {/* Mobile Nav */}
             <div className="sm:hidden h-16 justify-end flex sticky top-0 z-20"> 
-                {
+                {/* {
                     <a
                         href="/events/december"
                         className="px-6 py-2 rounded-full mt-4 mr-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring-1 ring-slate-800 flex items-center gap-x-4 text-sm"
@@ -136,7 +136,7 @@ export default function Nav() {
                             <ArrowRightIcon className="w-5 h-5" />
                         </div>
                     </a>
-                }
+                } */}
                 <div className="px-6 py-2 rounded-full mt-4 mr-4 bg-slate-900/50 backdrop-filter backdrop-blur-lg ring-1 ring-slate-800 flex items-center gap-x-4">
                     <a href="/">
                         <img
