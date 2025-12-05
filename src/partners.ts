9087222d5e37eb39083db5d7d413931851bf2ab9
Jerriel Chang <studybeetutoring@gmail.com>
2025-12-06 01:52:05 +0800

    Update partners for dec conf (#80)
    * Update partners for dec conf
    
    * added white logos
    
    idk why espressif is black on my preview but i checked the img i uploaded it was white
    
    * put cash sponsors on top
    
    ---------
    
    Co-authored-by: LinelyQwert <raynerng12@gmail.com>

--- src/partners.ts
+++ src/partners.ts
@@ -7,13 +7,60 @@ import revenuecat from "./img/partners/revenuecat.svg";
 import yubico from "./img/partners/yubico.svg";
 import nie from "./img/partners/nie.webp";
 import smu from "./img/partners/smu.png";
-
-export const curr_partners = [
+import htx from "./img/partners/htx.png";
+import csit from "./img/partners/csit.png";
+import ddas from "./img/partners/ddas.png";
+import espressif from "./img/partners/espressif.png";
+import wizardzines from "./img/partners/wizardzines.png";
+import singaprinting from "./img/partners/singaprinting.png";
+export const curr_partners = [    
+    {
+        src: htx,
+        title: "HTX",
+        desc: "HTX is a statutory board under the Ministry of Home Affairs that develops science and technology capabilities to empower the Home Team's operations and keep Singapore safe.",
+        website: "https://www.htx.gov.sg/",
+    },
+    {
+        src: csit,
+        title: "CSIT",
+        desc: "CSIT is a technical agency in the Ministry of Defence that harnesses cutting-edge digital technologies to meet Singapore's security needs.",
+        website: "https://www.csit.gov.sg/",
+    },
+    {
+        src: ddas,
+        title: "DDAS",
+        desc: "The Digital Defence Alliance Singapore (DDAS) is dedicated to strengthening Singapore's Digital Defence pillar by empowering enterprises and individuals with digital readiness and cyber resilience capabilities.",
+        website: "https://www.ddas.sg/",
+    },
     {
         src: smu,
         title: "Singapore Management University",
         desc: "A premier university in Asia, the Singapore Management University (SMU) is internationally recognised for its world-class research and distinguished teaching.",
         website: "https://smu.edu.sg",
+    },
+    {
+        src: github,
+        title: "GitHub",
+        desc: "GitHub is where you can store, share, and work together with others to write code. It provides distributed version control of access control, and more, for every project.",
+        website: "https://github.com/",
+    },
+    {
+        src: espressif,
+        title: "Espressif",
+        desc: "Espressif Systems is a multinational, fabless semiconductor company that develops low-power Wi-Fi and Bluetooth chips and modules for the Internet of Things (IoT).",
+        website: "https://www.espressif.com/",
+    },
+    {
+        src: wizardzines,
+        title: "Wizard Zines",
+        desc: "Wizard Zines are comics and zines by Julia Evans that make complex programming and technical concepts fun and accessible for everyone.",
+        website: "https://wizardzines.com/",
+    },
+    {
+        src: singaprinting,
+        title: "Singa Printing",
+        desc: "SingaPrinting is an online printing company in Singapore that specializes in producing high-quality custom stickers, labels, and business cards.",
+        website: "https://www.singaprinting.com/",
     },
     {
         src: youthtechsg,
@@ -54,12 +101,7 @@ export const prev_partners = [
         desc: "OKX is a Seychelles-based cryptocurrency exchange. As of Jan 2025, OKX is the 4th largest cryptocurrency exchange in the world according to CoinMarketCap.",
         website: "https://www.okx.com/",
     },
-    {
-        src: github,
-        title: "GitHub",
-        desc: "GitHub is where you can store, share, and work together with others to write code. It provides distributed version control of access control, and more, for every project.",
-        website: "https://github.com/",
-    },
+
     {
         src: sutd,
         title: "Singapore University of Technology and Design",
