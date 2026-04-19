8a117ef46ce4476f65194b015be8dae83b876941
myat <69038584+icepcp@users.noreply.github.com>
2026-04-19 22:48:48 +0800

    clean up past events and add newsletter form (#85)
    * clean up past events and add newsletter form
    
    * fix deploy error by adding types, clean up comments

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,58 +1,80 @@
 // docs.astro.build/en/guides/content-collections/#defining-a-collection-schema
 
-// 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
-// 2. Define a `type` and `schema` for each collection
-const eventsData = defineCollection({
-  type: 'data', // v2.5.0 and later
-  schema: z.object({
+export const eventSchema = z.object({
     title: z.string(),
     status: z.string(),
     partners: z.string().optional(),
     directions: z.string().optional(),
-    directionsData: z.array(z.object({
-      title: z.string(), 
-      tracks: z.array(z.record( 
-        z.array(z.object({
-          title: z.string(), 
-          directions: z.array(z.object({
-            method: z.string().optional(),
-            bold_method: z.string().optional(),
-            img: z.string(),
-            hyperlink: z.string().url().optional(),
-          }))
-        }))
-      ))
-    })).optional(),
+    directionsData: z
+        .array(
+            z.object({
+                title: z.string(),
+                tracks: z.array(
+                    z.record(
+                        z.array(
+                            z.object({
+                                title: z.string(),
+                                directions: z.array(
+                                    z.object({
+                                        method: z.string().optional(),
+                                        bold_method: z.string().optional(),
+                                        img: z.string(),
+                                        hyperlink: z.string().url().optional(),
+                                    }),
+                                ),
+                            }),
+                        ),
+                    ),
+                ),
+            }),
+        )
+        .optional(),
     luma: z.string().optional(),
     signupLink: z.string().optional(),
     details: z.string().array(),
     excerpt: z.string(),
     desc: z.string(),
-    faq: z.array(z.object({
-      q: z.string(),
-      a: z.string(),
-    })),
-    prospectus: z.object({
-      src: z.string()
-    }).array().optional(),
-    timeline: z.object({
-      title: z.string().optional(),
-      events: z.object({
-        date: z.string(),
-        event: z.object({
-          time: z.string().optional(),
-          title: z.string(),
-          desc: z.string().optional(),
-          author: z.string().optional(),
-        }).array()
-      }).array()
-    }).array(),
-  }),
+    faq: z.array(
+        z.object({
+            q: z.string(),
+            a: z.string(),
+        }),
+    ),
+    prospectus: z
+        .object({
+            src: z.string(),
+        })
+        .array()
+        .optional(),
+    timeline: z
+        .object({
+            title: z.string().optional(),
+            events: z
+                .object({
+                    date: z.string(),
+                    event: z
+                        .object({
+                            time: z.string().optional(),
+                            title: z.string(),
+                            desc: z.string().optional(),
+                            author: z.string().optional(),
+                        })
+                        .array(),
+                })
+                .array(),
+        })
+        .array(),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
+export type EventData = z.infer<typeof eventSchema>;
+
+const eventsData = defineCollection({
+    type: "data",
+    schema: eventSchema,
+});
+
 export const collections = {
-  'events': eventsData,
+    events: eventsData,
 };
